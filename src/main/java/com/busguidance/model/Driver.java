9eb7b616388af7e17a3c0c8d12448cc558b33434
4ever <ngoquockhanhquangu@gmail.com>
2026-06-01 22:19:27 +1000

    d1 setup

--- src/main/java/com/busguidance/model/Driver.java
+++ src/main/java/com/busguidance/model/Driver.java
@@ -41,4 +41,40 @@ public class Driver {
      this.birthDate = birthDate;
     }
 
+    // D1: Driver ID rule...   driverID must unique and no driverID duplicates //Driver Repo must check this **********
+    // driverID exactly 10 characters long, first 2 characters must be 2-9. atleast 2 special character 3-8. last 2 characters must uppercase A-Z
+
+    public boolean validateDriverID() {
+    if (driverID == null || driverID.length() != 10) {
+        return false;
+    }
+
+    // First 2 characters must be 2-9
+    if (!driverID.substring(0, 2).matches("[2-9]{2}")) {
+        return false;
+    }
+
+    // Last 2 characters must be A-Z
+    if (!driverID.substring(8, 10).matches("[A-Z]{2}")) {
+        return false;
+    }
+
+    // Characters 3 to 8 means index 2 to 7
+    String middlePart = driverID.substring(2, 8);
+
+    int specialCharacterCount = 0;
+
+    for (char c : middlePart.toCharArray()) {
+        if (!Character.isLetterOrDigit(c)) {
+            specialCharacterCount++;
+        }
+    }
+    if (specialCharacterCount < 2) {
+        return false;
+    }
+
+    return true;
+}
+       
+
 }
